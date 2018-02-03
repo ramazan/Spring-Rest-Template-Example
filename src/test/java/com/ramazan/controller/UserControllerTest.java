8053d069401d3077cc885d79bf8916f245857ec4
Ramazan Demir <ramazan.demir@sahibinden.com>
2018-02-03 19:45:54 +0300

    config circle ci

--- src/test/java/com/ramazan/controller/UserControllerTest.java
+++ src/test/java/com/ramazan/controller/UserControllerTest.java
@@ -1,29 +1,57 @@
 package com.ramazan.controller;
 
-import com.ramazan.services.ApiService;
+import com.ramazan.api.domain.Constants;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UserControllerTest
 {
 
     @Autowired
-    private ApiService apiService;
+    ApplicationContext applicationContext;
+
+    WebTestClient webTestClient;
 
     @Before
     public void setUp() throws Exception
     {
+        webTestClient = WebTestClient.bindToApplicationContext(applicationContext).build();
     }
 
     @Test
-    public void showUsers()
+    public void index() throws Exception
     {
 
+        webTestClient.get().uri("/")
+                .exchange()
+                .expectStatus().isOk();
     }
 
     @Test
-    public void formPost()
+    public void formPost() throws Exception
     {
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("limit", String.valueOf(Constants.USER_LIMIT));
+
+        webTestClient.post().uri("users")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(formData))
+                .exchange()
+                .expectStatus().isOk();
     }
+
 }
